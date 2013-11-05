8c751a4bf4348757d0128853114c3090a697aab7
sunsimon <tencent2015@gmail.com>
2013-11-05 17:05:43 +0800

    read config

--- bin/StartMasterServer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-chmod +x ./master_server
-mkdir -p ./log
-nohup ./master_server > ./log/master_server.stdout 2>&1 &
--- bin/StartServer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-chmod +x ./server
-mkdir -p ./log
-nohup ./server > ./log/server.stdout 2>&1 &
